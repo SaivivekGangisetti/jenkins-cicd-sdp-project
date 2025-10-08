8c7f527981f2342d37a44361147477cd9bcbaabd
VIVEK <saiv70034@gmail.com>
2025-10-08 21:48:02 +0530

    updated code

--- Ecommerce-backend/fooddelivery/src/main/java/com/fooddelivery/klu/FooddeliveryApplication.java
+++ Ecommerce-backend/fooddelivery/src/main/java/com/fooddelivery/klu/FooddeliveryApplication.java
@@ -2,9 +2,10 @@ package com.fooddelivery.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FooddeliveryApplication {
+public class FooddeliveryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FooddeliveryApplication.class, args);
